50b431834326f34ebdda0da5d29cca81b1c9570f
Abe <95333017+abeleinin@users.noreply.github.com>
2022-02-05 19:18:27 -0500

    mobile nav

--- toggleButton.js
+++ toggleButton.js
@@ -1,6 +1,8 @@
 const toggleButton = document.getElementById('toggle-button')
 const naviList = document.getElementById('navi-list')
+const waveId = document.getElementById('wave-id')
 
 toggleButton.addEventListener('click', () => {
     naviList.classList.toggle('active');
+    waveId.classList.toggle('none');
 })
